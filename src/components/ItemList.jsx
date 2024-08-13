4ec9de23ef4f0a90d7de197f0bc5ab258b2ff5f0
ShimaBell0619 <shimaoka0619@gmail.com>
2024-08-13 21:11:44 +0900

    レスポンシブに修正

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,6 @@
 export function ItemList(props) {
   return (
-    <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8  ">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-6 md:gap-8">
       {props.items.map((item, index) => {
         return (
           <div key={index} className="flex-none cursor-pointer ">
@@ -16,18 +16,17 @@ export function ItemList(props) {
                   "_ex=128x128",
                   "_ex=300x300"
                 )}
-                className="mb-5 rounded-md w-full transition-transform duration-300 group-hover:scale-105"
+                className="mb-3 sm:mb-5 rounded-md w-full transition-transform duration-300 group-hover:scale-105"
               />
             </a>
             <a
               href={item.itemUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-lg font-semibold text-blue-500 hover:underline"
+              className="text-sm sm:text-lg font-semibold text-blue-500 hover:underline"
             >
               {item.itemName}
             </a>
-            {/* <p className="text-gray-400">By {song.artists[0].name}</p> */}
           </div>
         );
       })}
